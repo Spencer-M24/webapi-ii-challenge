e821cb74c648e5d50b077a2afb5f7ad3023306d6
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-19 11:01:40 -0700

    created post and 200/400 sucess/fail

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ server.get('/api/posts',  (req, res) => {
 
 server.get('/api/posts/:id',  (req, res) => {
     const id = req.params.id;
+
+
+
+    db.findById(id)
+    .then(post => {
+        if(post.length) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The post information could not be retrieved'})
+    })
+});
+
+server.post('/api/posts',  (req, res) => {
+const postInfo = req.body;
