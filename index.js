33ee258558352afce9187c45efe91858d671e402
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-19 11:14:37 -0700

    Redoing Following Mvp Requirments

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-const express = require ('express');
+// Minimum Viable Product
+// Add the code necessary to implement the endpoints listed below.
+// Separate the endpoints that begin with /api/posts into a separate Express Router.
 
-const db = require('./data/db.js');
+// moduels port where to go and listen
 
-const server = express ();
+const server = require("./server.js");
 
+const port = 6900;
 
-server.use(express.json());
-
-
-
-server.get('/api/posts',  (req, res) => {
-    db.find()
-        .then(posts => {
-            res.status(200).json(posts);
-        })
-        .catch(err => {
-            res.status(500).res.json({ error: 'The posts information could not be retrieved'})
-        })
-})
-
-
-server.get('/api/posts/:id',  (req, res) => {
-    const id = req.params.id;
-
-
-
-    db.findById(id)
-    .then(post => {
-        if(post.length) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: 'The post with the specified ID does not exist.' })
-        }
-    })
-    .catch(err => {
-        res.status(500).json({ error: 'The post information could not be retrieved'})
-    })
-});
-
-server.post('/api/posts',  (req, res) => {
-const postInfo = req.body;
+server.listen(port, () => {});
